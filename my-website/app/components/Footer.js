3b5b34a6eb5893b5b8bccc84a5480f98751ba708
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2025-11-04 16:34:49 -0800

    Week 5 Demo

--- my-website/app/components/Footer.js
+++ my-website/app/components/Footer.js
@@ -1,7 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function Footer() {
-  return (
-    <footer className="border-t text-center p-4 text-sm font-semibold bg-primary-100 text-primary-900 border-primary-200">
-      <p>Me &copy; {new Date().getFullYear()}</p>
-    </footer>
-  );
+  const pathname = usePathname();
+  if (!pathname.startsWith("/studio")) {
+    return (
+      <footer className="border-t text-center p-4 text-sm font-semibold bg-primary-100 text-primary-900 border-primary-200">
+        <p>Me &copy; {new Date().getFullYear()}</p>
+      </footer>
+    );
+  }
 }
